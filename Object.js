8c82dcb0495bdf8231e47d24e0c8186153c1cdfd
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-08-20 18:41:51 +0700

    Javascript lanjutan video 2

--- Object.js
+++ Object.js
@@ -1,5 +1,6 @@
 // cara untuk membuat object pada javascript
 // 1. Object Litteral
+// PROBLEM : tidak efektif untuk object yang banyak
 // let mahasiswa1 = {
 //     nama: "Fadil Ardiansyah",
 //     energi: 10,
@@ -36,25 +37,50 @@
 //     return mahasiswa;
 // }
 
-// let fadil = Mahasiswa('Muhammad Fadil', 10);
-// let doddy = Mahasiswa('Doddy', 20);
-
-// 3. Constructor Function
-function Mahasiswa(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-
-    this.makan = function (porsi) {
+const methodMahasiswa = {
+    makan : function (porsi) {
         this.energi += porsi;
         console.log(`Halo ${this.nama}, Selamat makan`);
-    }
-
-    this.main = function (jam) {
+    },
+    main : function (jam) {
         this.energi -= jam;
         console.log(`Halo ${this.nama}, Energi berkurang`);
+    },
+    tidur : function (jam) {
+        this.energi += jam * 2;
+        console.log(`Halo ${this.nama}, Energi bertambah`);
     }
 }
 
-let fadil = new Mahasiswa('fadil',10);
+function Mahasiswa(nama, energi) {
+    let mahasiswa = Object.create(methodMahasiswa);
+    mahasiswa.nama = nama;
+    mahasiswa.energi = energi;
+    // mahasiswa.makan = methodMahasiswa.makan;
+    // mahasiswa.main = methodMahasiswa.main;
+    // mahasiswa.tidur = methodMahasiswa.tidur;
+    return mahasiswa;
+}
+
+let fadil = Mahasiswa('Muhammad Fadil', 10);
+let doddy = Mahasiswa('Doddy', 20);
+
+// 3. Constructor Function
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+
+//     this.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, Selamat makan`);
+//     }
+
+//     this.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, Energi berkurang`);
+//     }
+// }
+
+// let fadil = new Mahasiswa('fadil',10);
 
 // 4. Object Create
