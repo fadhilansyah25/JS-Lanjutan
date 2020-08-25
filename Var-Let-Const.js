a252f511db37b8d0c858b3c8b41bd22cb60833db
fadhilansyah25 <fadhilansyah25@gmail.com>
2020-08-25 21:08:36 +0700

    Javascript lanjutan video 6 Var, Let, & Const

---
+++ Var-Let-Const.js
@@ -0,0 +1,51 @@
+// console.log(i);
+// let i = 10;
+
+// console.log(i);
+// var i = 10;
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+
+// const datadiri = [
+//     {
+//         nama: "Fadil Ardiansyah",
+//         umur: 23,
+//         alamat: "Tangerang"
+//     },
+//     {
+//         nama: "Rudi",
+//         umur: 23,
+//         alamat: "Tangerang"
+//     },
+//     {
+//         nama: "Hartono",
+//         umur: 23,
+//         alamat: "Tangerang"
+//     }
+// ]
+
+// datadiri.push({
+//     nama: "Agung",
+//     umur: 33,
+//     alamat: "Jakarta"
+// });
+
+// datadiri.forEach((value) => {
+//     console.log(`${value.nama}, ${value.umur}, ${value.alamat}`);
+// })
+
+// let a = 10;
+// a += 10;
+// console.log(a);
+
+// const b = 10;
+// b += 10;
+// console.log(b);
